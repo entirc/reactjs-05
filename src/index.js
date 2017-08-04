870002992d07509eae0733f884da3a4d01a65f3a
michel adriano medeiros <michel adriano medeiros>
2017-08-04 17:20:28 -0300

    Segundo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 
 class Person extends React.Component{
   constructor () {
+  //  console.log("Constructor")
     super()
     this.state = {
       email: '',
@@ -10,17 +11,18 @@ class Person extends React.Component{
     }
   }
 
-  //adicionando atributos no props
   static get defaultProps() {
+  //  console.log("defaultProps")
     return {
         name: '????',
         age: 35
     }
   }
 
+
   componentWillMount() {
     let mode;
-    //this.setState({ foo: 'bar' });
+    this.setState({ foo: 'bar' });
     //console.log("Node: ", ReactDOM.findDOMNode(this.refs.titulo))
    // console.log("this willMount: ", this)
     if (this.props.age > 80) {
@@ -33,38 +35,73 @@ class Person extends React.Component{
     this.setState({ mode });
   }
 
+
   componentDidMount() {
     //console.log("Node Título: ", ReactDOM.findDOMNode(this.refs.titulo))
     //console.log("Node Livro: ", ReactDOM.findDOMNode(this.refs.livro))
   }
 
+  /************* Segunda Fase ********************/
+
   componentWillReceiveProps(nextProps) {
-    console.log("this.props.email ",this.props.email)
-    console.log("componentWillReceiveProps: ",nextProps.email)
-    this.setState({
+    //console.log("this.props.email ",this.props.email)
+    //console.log("componentWillReceiveProps: ",nextProps.email)
+   // console.log(this.props)
+    //this.props.name = "Michel";
+
+  /*  this.setState({
       email:nextProps.email
-    })
+    })*/
   }
 
-
   shouldComponentUpdate(nextProps, nextState){
-    console.log("shouldComponentUpdate nextProps ",nextProps)
-    console.log("shouldComponentUpdate nextState ",nextState)
-    if(this.props!=nextProps || this.state!=nextState){
+    //console.log("shouldComponentUpdate nextProps ",nextProps)
+   // console.log("shouldComponentUpdate nextState ",nextState)
+/*
+    this.setState({
+      email:nextState.email="TESTE"
+    })
+*/
+ /*   if(this.props.email!=nextProps.email || this.state.email!=nextState.email){
+     // console.log('TRUE')
       return true
     }else{
       return false
-    }
+    }*/
+
+  // return false
   }
 
+  componentWillUpdate(nextProps, nextState){
+   // console.log("componentWillUpdate nextProps ",nextProps)
+   // console.log("email valor no input ", this.state.email)
+   // console.log("componentWillUpdate nextState ",nextState)
+    //let temArroba = (nextState.email).includes("@")
+    //console.log("temArroba " + temArroba)
+/*
+    this.setState({
+      email:nextState.email="TESTE"
+    })
+   */
+  }
+
+  componentDidUpdate(prevProps, prevState){
+   // console.log("componentDidUpdate prevProps ",prevProps)
+    //console.log("email valor no input ", this.state.email)
+   // console.log("componentDidUpdate prevState ",prevState)
+  }
+
+  /************* Terceira Fase ********************/
+
+
   render(){
-   // console.log("this Person: ", this)
-    console.log("this.props: ", this.props)
-    console.log("this.state: ", this.state)
+    console.log("this Person: ", this)
+    console.log("render this.props: ", this.props)
+    console.log("render this.state: ", this.state)
     return(
       <div>  
         <h1 ref="titulo"> ENTI - Aprendendo React #5 </h1>
-        <p>O email é: {this.state.email}</p>
+ 
       </div>
     )
   }
@@ -74,21 +111,58 @@ class Person extends React.Component{
 class Form extends React.Component {
   constructor(props) {
     super(props);
-    this.state        = { email: '' } ;
+    this.state = { email: '' } ;
     this.handleChange = this.handleChange.bind(this);
   }
 
+  componentWillUnmount(){
+    console.log("componentWillUnmount ")
+  }
+
   handleChange(event) {
-    this.setState({ email: event.currentTarget.value });
+    let valorDigitado = event.currentTarget.value
+    this.setState({ email: valorDigitado });
+   // console.log("AQUI ",  ReactDOM.findDOMNode(document.getElementById("app")))
+   // ReactDOM.unmountComponentAtNode(ReactDOM.findDOMNode(document.getElementById("app")))
+  }
+
+  montar(){
+    ReactDOM.render(
+      <div>
+        <MeuNome />
+      </div>,
+      document.getElementById('meuNome')
+    )
+  }
+
+  desmontar(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('meuNome'))
   }
 
   render() {
+    //console.log("Render Form")
     return (
       <div>
-        <label>email: </label><input type="text" onChange={ this.handleChange } />
-        <Person email={ this.state.email } />
+        <button onClick={this.montar}>Montar</button>&nbsp;
+        <button onClick={this.desmontar}>Desmontar</button>&nbsp;
+        <div id="meuNome"></div>
+        <br/>
+        <label id="labelEmail" ref="em">email: </label><input type="text" onChange={ this.handleChange } />
+        <Person />
       </div>
     );
+  }
+}
+
+class MeuNome extends React.Component{
+  componentWillUnmount(){
+    console.log("desmontou o componente ")
+  }
+
+  render(){
+    return(
+      <h1>Michel Adriano Medeiros</h1>
+    )
   }
 }
 
@@ -113,6 +187,10 @@ class Links extends React.Component{
         <br/><br/>
         <img src="/assets/images/stateChange.png" />&nbsp;
         <img src="/assets/images/propsChange.png" />
+        <br/><br/>
+        <img src="/assets/images/unmount.png" />
+        <br/><br/>
+        <img src="/assets/images/react-lifecycle-flow-chart-states.png" />
         <br/><br/>
         <a href="https://tylermcginnis.com/an-introduction-to-life-cycle-events-in-react-js/" target="_">An Introduction to Life Cycle Events in React</a>
         <br/>
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: [
     path.resolve(__dirname, 'src', 'index.js'),
-    'webpack-dev-server/client?http://localhost:9050'
+    'webpack-dev-server/client?http://localhost:8080'
   ],
   output: {
     filename: 'bundle.js',
